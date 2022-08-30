87a1085186807b090a5022efc8f6014fd3b8c216
Sam Devine <samuel.will.devine@gmail.com>
2022-08-30 15:17:31 -0600

    test: basics

---
+++ tests/test_retirement_calculator.py
@@ -0,0 +1,69 @@
+from user import User
+from retirement_calculator import RetirementCalculator
+
+
+calc = RetirementCalculator(
+    {
+        "user_info": {
+            "date_of_birth": "1975-09-02",
+            "household_income": 224726,
+            "current_savings_rate": 9,
+            "current_retirement_savings": 15518,
+            "full_name": "Danny Kane",
+            "address": "51495 Smith Crest\nNorth Samanthaview, AL 35953",
+        },
+        "assumptions": {
+            "pre_retirement_income_percent": 67,
+            "life_expectancy": 95,
+            "expected_rate_of_return": 10,
+            "retirement_age": 58,
+        },
+    }
+)
+
+
+def test_calculator_exists():
+    assert type(calc) == RetirementCalculator
+
+
+def test_calculator_has_attributes():
+    assert calc.annual_inflation_rate == 0.03
+    assert calc.annual_salary_increase == 0.02
+    assert type(calc.user) == User
+
+
+def test_calculate_saving_enough():
+    calc.user.current_savings_rate = 100
+    assert calc.calculate() == True
+
+
+def test_calculate_not_saving_enough():
+    calc.user.current_savings_rate = 9
+    assert calc.calculate() == False
+
+
+def test_amount_needed_at_retirement():
+    assert calc._amount_needed_at_retirement(50000) == 436552
+    assert calc._amount_needed_at_retirement(100000) == 873110
+    assert calc._amount_needed_at_retirement(1000000) == 8731143
+
+
+def test_future_withdrawal():
+    assert calc._future_withdrawal(100000, 0) == 100000
+    assert calc._future_withdrawal(100000, 5) == 115927
+    assert calc._future_withdrawal(100000, 10) == 134391
+
+
+def test_expected_savings_at_retirement():
+    assert calc._expected_savings_at_retirement() == {
+        "balance": 521516,
+        "pre_retirement_income": 279418,
+    }
+
+    calc.user.current_savings_rate = 40
+    calc.user.household_income = 60000
+
+    assert calc._expected_savings_at_retirement() == {
+        "balance": 609758,
+        "pre_retirement_income": 74602,
+    }
---
+++ tests/test_user.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from user import User
+
+
+user = User(
+    {
+        "user_info": {
+            "date_of_birth": "1975-09-02",
+            "household_income": 224726,
+            "current_savings_rate": 9,
+            "current_retirement_savings": 15518,
+            "full_name": "Danny Kane",
+            "address": "51495 Smith Crest\nNorth Samanthaview, AL 35953",
+        },
+        "assumptions": {
+            "pre_retirement_income_percent": 67,
+            "life_expectancy": 95,
+            "expected_rate_of_return": 10,
+            "retirement_age": 58,
+        },
+    }
+)
+
+
+def test_user_exists():
+    assert type(user) == User
+
+
+def test_user_has_attributes():
+    assert user.date_of_birth == "1975-09-02"
+    assert user.household_income == 224726
+    assert user.current_savings_rate == 9
+    assert user.current_retirement_savings == 15518
+    assert user.full_name == "Danny Kane"
+    assert user.address == "51495 Smith Crest\nNorth Samanthaview, AL 35953"
+    assert user.pre_retirement_income_percent == 67
+    assert user.life_expectancy == 95
+    assert user.expected_rate_of_return == 10
+    assert user.retirement_age == 58
+
+
+def test_years_until_retirement():
+    dob = datetime.strptime(user.date_of_birth, "%Y-%m-%d")
+    assert dob == datetime(1975, 9, 2, 0, 0)
+
+    age = int((datetime.today() - dob).days / 365.25)
+    assert user.years_until_retirement() == user.retirement_age - age
